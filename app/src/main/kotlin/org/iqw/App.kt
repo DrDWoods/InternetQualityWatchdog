387ceb620b4a0adc8088465a2534c26b37cb3e23
dr.david.j.l.woods@gmail.com <dr.david.j.l.woods@gmail.com>
2024-10-04 16:57:55 +0100

    Added some tests

--- app/src/main/kotlin/org/iqw/App.kt
+++ app/src/main/kotlin/org/iqw/App.kt
@@ -1,5 +1,6 @@
 package org.iqw
 
+import org.iqw.bin.Executor
 import org.iqw.database.DatabaseCreator
 import org.iqw.database.SpeedTable
 import org.iqw.database.SpeedTableConfig
@@ -54,5 +55,12 @@ class App {
 fun main() {
     val username = System.getenv("MySQLUsername")
     val password = System.getenv("MySQLPassword")
-    App().run(username, password)
+    //App().run(username, password)
+
+    val executor = Executor()
+    executor.execute("notacommand")
+    //executor.execute("cmd", "/c", "echo", "somewords")
+    val output = executor.output.length + executor.error.length
+    println(executor.output.isEmpty())
+    println("OUTPUT: $output")
 }
--- app/src/main/kotlin/org/iqw/bin/Executor.kt
+++ app/src/main/kotlin/org/iqw/bin/Executor.kt
@@ -1,16 +1,17 @@
 package org.iqw.bin
 
+import java.io.IOException
+
 /**
  * Execute a binary and store stdout and stderr
  */
-class Executor() {
+class Executor {
 
     var output: String = ""
     var error: String = ""
 
     fun execute(path: String, vararg args: String){
         try {
-
             // Use ProcessBuilder to run the command
             val process = ProcessBuilder(path, *args).start()
 
@@ -18,12 +19,16 @@ class Executor() {
             output = String(process.inputStream.readAllBytes());
             error = String(process.errorStream.readAllBytes());
 
-            if (error.length != 0)
-                throw Exception("Executable call failed.")
+            if (output.isEmpty() && error.isEmpty()){
+                throw IOException("Command $path not found")
+            }
 
-        } catch (e: Exception) {
-            e.printStackTrace()
-            print(error)
+            if (error.length != 0)
+                throw IOException("Command returned an error: $error")
+
+        } catch (exception: IOException) {
+            "IOException occurred: ${exception.message}"
+            throw exception
         }
     }
 }
